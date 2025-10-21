df871926ad9fabcdcf9c1388f328050a7b3ad424
Bruno Simbine <brunosimbine07012002@gmail.com>
2025-10-21 15:48:23 +0200

    sdhsiufdsf

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { setToken, clearToken, getToken } from '@/services/auth';
 
 const api = axios.create({
-  baseURL: 'https:/api.faturizze.com/v1',
+  baseURL: 'https://api.faturizze.com/v1',
   headers: {
     'Content-Type': 'application/json',
   },
